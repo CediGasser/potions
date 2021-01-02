8ce37e20f6913a895188fdb868616b3b0a1152cf
Cedric Gasser <cedric_gasser@sluz.ch>
2021-01-03 00:39:09 +0100

    Implemented "templating"

--- src/backend/fileserver.ts
+++ src/backend/fileserver.ts
@@ -1,12 +1,23 @@
 import { send, Context } from "https://deno.land/x/oak@v6.4.0/mod.ts";
 
 export const fileserver = async (context: Context<Record<string, any>>) => {
-    await send(
-        context,
-        context.request.url.pathname,
-        { 
-            root: `${Deno.cwd()}/src/frontend`,
-            index: "index.html"
-        }
-    );
+    
+    const frontendPath = "/src/frontend"
+
+    if(context.request.url.pathname.endsWith(".html")){
+        const template = await Deno.readTextFile(`${Deno.cwd()}${frontendPath}/template.html`);
+        const page = await Deno.readTextFile(`${Deno.cwd()}${frontendPath}${context.request.url.pathname}`);
+
+        context.response.body = template.replace("<!-- Here goes the content -->", page);
+
+    } else{
+        await send(
+            context,
+            context.request.url.pathname,
+            { 
+                root: `${Deno.cwd()}${frontendPath}`,
+                index: "index.html"
+            }
+        );
+    }
 };
