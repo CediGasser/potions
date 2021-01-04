635fdebe1ffe8d8a4dfe263547ec460c008410b9
Cedric Gasser <cedric_gasser@sluz.ch>
2021-01-04 20:42:50 +0100

    Fixed bug where template would not load on path "/"

--- src/backend/fileserver.ts
+++ src/backend/fileserver.ts
@@ -2,14 +2,17 @@ import { send, Context } from "https://deno.land/x/oak@v6.4.0/mod.ts";
 
 export const fileserver = async (context: Context<Record<string, any>>) => {
     
-    const frontendPath = "/src/frontend"
+    const frontendPath = "/src/frontend";
 
+    if(context.request.url.pathname == "/"){
+        context.request.url.pathname = "/index.html";
+    }
     if(context.request.url.pathname.endsWith(".html")){
         const template = await Deno.readTextFile(`${Deno.cwd()}${frontendPath}/template.html`);
         const page = await Deno.readTextFile(`${Deno.cwd()}${frontendPath}${context.request.url.pathname}`);
 
         context.response.body = template.replace("<!-- Here goes the content -->", page);
-
+        
     } else{
         await send(
             context,
