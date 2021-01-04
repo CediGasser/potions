8a0b07fcd53a265d97e11475db8e87904cbfebb8
Cedric Gasser <cedric_gasser@sluz.ch>
2021-01-04 20:18:58 +0100

    Created basic Overview and Detail page functionality

--- src/frontend/detail.ts
+++ src/frontend/detail.ts
@@ -2,8 +2,15 @@
 
 import { Potion } from "../common/types.ts";
 
-export async function loadDetail(id: number) {
+export async function loadDetail() {
+    const id = new URLSearchParams(window.location.search).get("potionId"); 
     const response = await fetch(`/api/potions/${id}`);
     const potion: Potion = await response.json();
+
     console.log(potion);
+
+    document.getElementById("potionName").textContent = potion.name;
+    document.getElementById("potionImage").setAttribute("src", potion.image);
+    document.getElementById("potionDescription").textContent = potion.description;
+    document.getElementById("potionPrice").textContent = "Costs: " + potion.price + " Emeralds";
 }
--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -5,5 +5,36 @@ import { Potion } from "../common/types.ts";
 export async function loadPotions() {
     const response = await fetch("/api/potions");
     const potions: Potion[] = await response.json();
+    
     console.log(potions);
+
+    const overviewDiv = document.getElementById("potionOverview");
+    potions.forEach(potion => {
+        overviewDiv.appendChild(createPotionCard(potion));
+    });
+}
+
+function createPotionCard(potion: Potion): Node{
+    const div = document.createElement("div");
+    const a = document.createElement("a");
+    const img = document.createElement("img");
+    div.appendChild(img);
+    const textDiv = document.createElement("div");
+    div.appendChild(textDiv);
+    const text = document.createElement("p");
+    textDiv.appendChild(text);
+    div.appendChild(a);
+
+    div.setAttribute("class", "potionCard");
+
+    a.href = `detail.html?potionId=${potion.id}`;
+
+    img.setAttribute("class", "potionCardImg");
+    img.src = potion.image;
+
+    textDiv.setAttribute("class", "potionCardTextDiv");
+
+    text.innerText = potion.name;
+
+    return div;
 }
